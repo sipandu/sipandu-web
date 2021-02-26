aacb2c55fd99719145fa5fef72885921db1b5bb0
Hadi-Darmawan <ghadi.darmawan@gmail.com>
2021-02-26 18:38:20 +0800

    Mengubah icon pada sidebar dan facivon

--- resources/views/layouts/admin/admin-layout.blade.php
+++ resources/views/layouts/admin/admin-layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" href="{{ asset('sipandu.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('/images/sipandu-logo.ico') }}">
     <title>SIPANDU | Admin | @yield('title')</title>
 
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
