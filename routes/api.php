ff5d2da5f879690d6bfcc9ba82db539c8817cb4c
unknown <reybernard1511@gmail.com>
2021-03-31 23:43:43 +0800

    Reviewing Logout

--- routes/api.php
+++ routes/api.php
@@ -23,12 +23,14 @@ Route::middleware('auth:sanctum')->get('/test', function (Request $request){
     return response()->json(Ibu::all());
 });
 
-Route::post('/Login', 'User\Auth\Api\ApiLoginController@login');
-Route::get('/Logout', 'User\Auth\Api\ApiLoginController@logout')->middleware('auth:sanctum');
-Route::post('/register', 'User\Auth\Api\ApiRegisterController@firstRegis');
-Route::post('/register-anak', 'User\Auth\Api\ApiRegisterController@storeAnak');
-Route::post('/register-ibu', 'User\Auth\Api\ApiRegisterController@storeIbu');
-Route::post('/register-lansia', 'User\Auth\Api\ApiRegisterController@storeLansia');
+Route::prefix('mobileuser')->group(function(){
+    Route::post('/Login', 'User\Auth\Api\ApiLoginController@login');
+    Route::get('/Logout', 'User\Auth\Api\ApiLoginController@logout')->middleware('auth:sanctum');
+    Route::post('/register', 'User\Auth\Api\ApiRegisterController@firstRegis');
+    Route::post('/register-anak', 'User\Auth\Api\ApiRegisterController@storeAnak');
+    Route::post('/register-ibu', 'User\Auth\Api\ApiRegisterController@storeIbu');
+    Route::post('/register-lansia', 'User\Auth\Api\ApiRegisterController@storeLansia');
+});
 
 Route::get('/regist-data-posyandu', 'User\Auth\Api\ApiRegisterController@getAllPosyandu');
 
