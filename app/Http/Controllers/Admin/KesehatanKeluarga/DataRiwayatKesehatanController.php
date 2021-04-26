e44274287c151b74f3a757c9d8f35624f23eccda
unknown <reybernard1511@gmail.com>
2021-04-26 18:29:24 +0800

    Grafik-bumil 20%

--- app/Http/Controllers/Admin/KesehatanKeluarga/DataRiwayatKesehatanController.php
+++ app/Http/Controllers/Admin/KesehatanKeluarga/DataRiwayatKesehatanController.php
@@ -9,6 +9,8 @@ use App\User;
 use App\Anak;
 use App\Ibu;
 use App\Lansia;
+use App\PemeriksaanIbu;
+use App\PemeriksaanAnak;
 
 class DataRiwayatKesehatanController extends Controller
 {
@@ -48,6 +50,21 @@ class DataRiwayatKesehatanController extends Controller
 
     public function kesehatanIbu(Ibu $ibu)
     {
+        $dataAwal = PemeriksaanIbu::where('id_ibu_hamil', $ibu->id)->orderBy('created_at', 'asc')->first();
+        $beratAwal = $dataAwal->berat_badan;
+        $dataPemeriksaan = PemeriksaanIbu::where('id_ibu_hamil', $ibu->id)->orderBy('created_at', 'asc')->get();
+        $awal[] = null;
+        $menengah[] = null;
+        $menengah2[] = null;
+        $akhir[] = null;
+        foreach($dataPemeriksaan as $d){
+            if($d->usia_kandungan < 18.5 ){
+                array_push($awal, $d->berat_badan);
+            }elseif($d->usia_kandungan > 10.5 and $d->usia_kandungan < 24.9 ){
+
+            }
+        }
+        // dd($dataPemeriksaan[0]->berat_badan);
         return view('pages/admin/kesehatan-keluarga/data-kesehatan/data-kesehatan-ibu');
     }
 
--- resources/views/pages/admin/kesehatan-keluarga/data-kesehatan/data-kesehatan-ibu.blade.php
+++ resources/views/pages/admin/kesehatan-keluarga/data-kesehatan/data-kesehatan-ibu.blade.php
@@ -313,8 +313,8 @@
                             display: true,
                             text: 'Value'
                         },
-                        // suggestedMin: -1,
-                        // suggestedMax: 23,
+                        suggestedMin: 0,
+                        suggestedMax: 23,
                         // labels: [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
                     }
                 }
