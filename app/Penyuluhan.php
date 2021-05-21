d204878b23aa035da6d3607540d4a5fb6ebdf23c
I Gede Aldi Wahyudi <aldiwahyudi@student.unud.ac.id>
2021-05-22 06:54:25 +0800

    Fix Bug

--- app/Penyuluhan.php
+++ app/Penyuluhan.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\DB;
 
 class Penyuluhan extends Model
 {
@@ -22,14 +23,53 @@ class Penyuluhan extends Model
                 'Tanggal : '.$this->tanggal.PHP_EOL.
                 'Topik : '.$this->topik_penyuluhan.PHP_EOL.
                 'Deskripsi Kegiatan : '.PHP_EOL.PHP_EOL.
-                $this->deskripsi;
-        $token = '1519375290:AAEt7FLKWTlaEPJgamcqKv8pVTtboTbA9iY';
+                strip_tags($this->deskripsi);
+        $token = '1137522342:AAEj3X4Obbi-uV8QGzkvcvpzjo6HKENKfX4';
         $url = 'https://api.telegram.org/bot'.$token.'/sendMessage';
+
+        $anak = DB::table('tb_user')->join('tb_anak', 'tb_anak.id_user', 'tb_user.id')
+            ->select('tb_user.id_chat_tele')
+            ->where('tb_user.id_chat_tele', '!=', NULL)
+            ->where('tb_anak.id_posyandu', $this->id_posyandu)
+            ->get();
+        $ibu_hamil = DB::table('tb_user')->join('tb_ibu_hamil', 'tb_ibu_hamil.id_user', 'tb_user.id')
+            ->select('tb_user.id_chat_tele')
+            ->where('tb_user.id_chat_tele', '!=', NULL)
+            ->where('tb_ibu_hamil.id_posyandu', $this->id_posyandu)
+            ->get();
+        $lansia = DB::table('tb_user')->join('tb_lansia', 'tb_lansia.id_user', 'tb_user.id')
+            ->select('tb_user.id_chat_tele')
+            ->where('tb_user.id_chat_tele', '!=', NULL)
+            ->where('tb_lansia.id_posyandu', $this->id_posyandu)
+            ->get();
+
+        foreach($anak as $item)
+        {
+            $response = Http::post($url, [
+                'chat_id' => $item->id_chat_tele,
+                'text' => $msg,
+            ]);
+            sleep(2);
+        }
+
+        foreach($lansia as $item)
+        {
+            $response = Http::post($url, [
+                'chat_id' => $item->id_chat_tele,
+                'text' => $msg,
+            ]);
+            sleep(2);
+        }
+
+        foreach($ibu_hamil as $item)
+        {
+            $response = Http::post($url, [
+                'chat_id' => $item->id_chat_tele,
+                'text' => $msg,
+            ]);
+            sleep(2);
+        }
         // $chat_id = $this->posyandu->id_chat_grup_tele;
-        $response = Http::post($url, [
-            'chat_id' => '-518331855',
-            'text' => $msg,
-        ]);
     }
 
     public function sendMessageRalat()
@@ -40,13 +80,52 @@ class Penyuluhan extends Model
                 'Tanggal : '.$this->tanggal.PHP_EOL.
                 'Topik : '.$this->topik_penyuluhan.PHP_EOL.
                 'Deskripsi Kegiatan : '.PHP_EOL.PHP_EOL.
-                $this->deskripsi;
-        $token = '1519375290:AAEt7FLKWTlaEPJgamcqKv8pVTtboTbA9iY';
+                strip_tags($this->deskripsi);
+        $token = '1137522342:AAEj3X4Obbi-uV8QGzkvcvpzjo6HKENKfX4';
         $url = 'https://api.telegram.org/bot'.$token.'/sendMessage';
         // $chat_id = $this->posyandu->id_chat_grup_tele;
-        $response = Http::post($url, [
-            'chat_id' => '-518331855',
-            'text' => $msg,
-        ]);
+
+        $anak = DB::table('tb_user')->join('tb_anak', 'tb_anak.id_user', 'tb_user.id')
+            ->select('tb_user.id_chat_tele')
+            ->where('tb_user.id_chat_tele', '!=', NULL)
+            ->where('tb_anak.id_posyandu', $this->id_posyandu)
+            ->get();
+        $ibu_hamil = DB::table('tb_user')->join('tb_ibu_hamil', 'tb_ibu_hamil.id_user', 'tb_user.id')
+            ->select('tb_user.id_chat_tele')
+            ->where('tb_user.id_chat_tele', '!=', NULL)
+            ->where('tb_ibu_hamil.id_posyandu', $this->id_posyandu)
+            ->get();
+        $lansia = DB::table('tb_user')->join('tb_lansia', 'tb_lansia.id_user', 'tb_user.id')
+            ->select('tb_user.id_chat_tele')
+            ->where('tb_user.id_chat_tele', '!=', NULL)
+            ->where('tb_lansia.id_posyandu', $this->id_posyandu)
+            ->get();
+
+        foreach($anak as $item)
+        {
+            $response = Http::post($url, [
+                'chat_id' => $item->id_chat_tele,
+                'text' => $msg,
+            ]);
+            sleep(2);
+        }
+
+        foreach($lansia as $item)
+        {
+            $response = Http::post($url, [
+                'chat_id' => $item->id_chat_tele,
+                'text' => $msg,
+            ]);
+            sleep(2);
+        }
+
+        foreach($ibu_hamil as $item)
+        {
+            $response = Http::post($url, [
+                'chat_id' => $item->id_chat_tele,
+                'text' => $msg,
+            ]);
+            sleep(2);
+        }
     }
 }
