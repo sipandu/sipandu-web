45e343c0863e020359a578cab2dfe59eb28d4536
Hadi Darmawan <ghadi.darmawan@gmail.com>
2021-06-14 22:06:06 +0800

    Menambahkan error page sederhana

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,5 @@
 @extends('errors::minimal')
 
-@section('title', __('Not Found'))
+@section('title', __('Halaman Tidak Ditemukan'))
 @section('code', '404')
-@section('message', __('Not Found'))
+@section('message', __('Halaman Tidak Ditemukan'))
